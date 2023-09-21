da1a136e81b349a9516f4fa3a4b9cc0005139f4a
Kosta Korenkov <kosta@korenkov.net>
2023-09-21 18:10:40 +0200

    temporary hide top navigation as we don't have content for these pages yet

--- _data/navigation.js
+++ _data/navigation.js
@@ -1,30 +1,30 @@
 module.exports = [
-  {
-    slug: 'businesses',
-    label: {
-      'ru': 'Бизнес-сообщество',
-      'en': 'Local businesses'
-    }
-  },
-  {
-    slug: 'city',
-    label: {
-      'ru': 'Монтелиберо Сити',
-      'en': 'Montelibero City'
-    }
-  },
-  {
-    slug: 'tokenomics',
-    label: {
-      'ru': 'Токеномика',
-      'en': 'Tokenomics'
-    }
-  },
-  {
-    slug: 'montenegro',
-    label: {
-      'ru': 'Релокация',
-      'en': 'Moving to Montenegro'
-    }
-  }
+  // {
+  //   slug: 'businesses',
+  //   label: {
+  //     'ru': 'Бизнес-сообщество',
+  //     'en': 'Local businesses'
+  //   }
+  // },
+  // {
+  //   slug: 'city',
+  //   label: {
+  //     'ru': 'Монтелиберо Сити',
+  //     'en': 'Montelibero City'
+  //   }
+  // },
+  // {
+  //   slug: 'tokenomics',
+  //   label: {
+  //     'ru': 'Токеномика',
+  //     'en': 'Tokenomics'
+  //   }
+  // },
+  // {
+  //   slug: 'montenegro',
+  //   label: {
+  //     'ru': 'Релокация',
+  //     'en': 'Moving to Montenegro'
+  //   }
+  // }
 ];
